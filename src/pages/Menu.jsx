18e11b32c70572f822fcb1b3f570f1d3bc195655
Roberto Lovera <rlovera1cursos@gmail.com>
2023-08-13 03:01:52 -0400

    se modifico menu

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,4 +1,16 @@
  import image1 from "../img/icono_menu.jpg";
+ import image_principal from "../img/sombrero-menu.jpg";
+ import image_bolonesa from "../img/Tagliatelle-al-ragu-alla-Bolognese.jpg";
+ import image_carbonara from "../img/carbonara.jpg";
+ import image_putanesca from "../img/putanesca.jpg";
+ import image_ravioli from "../img/Ravioli-53.jpg";
+ import image_piemontese from "../img/risoto-piemontese.jpg";
+ import image_alnegro from "../img/risono-al-negro.jpg";
+ import image_heladomantecado from "../img/helado-mantecado.jpg";
+ import image_heladochocolate from "../img/helado-chocolate.jpg";
+ import image_semifredo from "../img/semifredo.jpg";
+ import image_gelatotartufo from "../img/gelato-tartufo.jpg";
+ import image_crepnutella from "../img/crep-nutella.jpg";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -23,12 +35,26 @@ const Menu = () => {
 //     );
 
 // }
-    return (
+
+  //************CAMBIANDO IMAGEN PRINCIPAL CUANDO TIENE EL FOCO ***************/
+  const cambiarImagenOn = (id,img) =>{
+            
+    document.getElementById(id).src=img;
+ };
+
+//************CAMBIANDO IMAGEN PRINCIPAL CUANDO PIERDE EL FOCO ***************/
+const cambiarImagenOff = (id,img) =>{
+          
+  document.getElementById(id).src=img;
+};
+ 
+
+return (
       <>
       <Header />
         <main className="row">
           <article className="col">
-
+         
 
             <br />
             <table><tr><td>
@@ -40,16 +66,37 @@ const Menu = () => {
             </table>
             <br />
             {/* {imprimirMenu} */}
+            
+          <div className="contenedor_menu">
             <table>
                 <tr><td className="celdas_tabla_menu">
                   <span className="subtituloTabla">
                     Pastas
                   </span>
              <ul>
-                <li>Bolo&ntilde;esa</li>
-                <li>Carbonara</li>
-                <li>Putanesca</li>
-                <li>Ravioli</li>
+                <li>
+                  <a href="#" 
+                 onMouseOver={() => cambiarImagenOn('platos_fuertes',image_bolonesa)}  
+                 onMouseOut={() => cambiarImagenOff('platos_fuertes',image_principal)}>
+                 Bolo&ntilde;esa</a></li>
+
+                <li>
+                <a href="#" 
+                 onMouseOver={() => cambiarImagenOn('platos_fuertes',image_carbonara)}  
+                 onMouseOut={() => cambiarImagenOff('platos_fuertes',image_principal)}>
+                 Carbonara</a></li>
+
+                <li>
+                <a href="#" 
+                 onMouseOver={() => cambiarImagenOn('platos_fuertes',image_putanesca)}  
+                 onMouseOut={() => cambiarImagenOff('platos_fuertes',image_principal)}>
+                  Putanesca</a></li>
+
+                <li>
+                  <a href="#"
+                onMouseOver={() => cambiarImagenOn('platos_fuertes',image_ravioli)}  
+                onMouseOut={() => cambiarImagenOff('platos_fuertes',image_principal)}>
+                  Ravioli</a></li>
             </ul>
             </td>
 
@@ -58,8 +105,17 @@ const Menu = () => {
                 Risottos
               </span>
             <ul>
-                <li>Piemomtese</li>
-                <li>Al negro</li>
+                <li>
+                <a href="#"
+                onMouseOver={() => cambiarImagenOn('platos_fuertes',image_piemontese)}  
+                onMouseOut={() => cambiarImagenOff('platos_fuertes',image_principal)}>
+                  Piemomtese</a></li>
+
+                <li>
+                <a href="#"
+                onMouseOver={() => cambiarImagenOn('platos_fuertes',image_alnegro)}  
+                onMouseOut={() => cambiarImagenOff('platos_fuertes',image_principal)}>
+                  Al negro</a></li>
             </ul>
             </td>
             
@@ -68,19 +124,46 @@ const Menu = () => {
               Postres
             </span>
             <ul>
-                <li>Helado Mantecado</li>
-                <li>Helado Chocolate</li>
-                <li>Semifredo</li>
-                <li>Gelato al Tartufo</li>
-                <li>Crep de Nutella</li>
+                <li>
+                <a href="#"
+                onMouseOver={() => cambiarImagenOn('platos_fuertes',image_heladomantecado)}  
+                onMouseOut={() => cambiarImagenOff('platos_fuertes',image_principal)}>
+                  Helado Mantecado</a></li>
+
+                <li>
+                <a href="#"
+                onMouseOver={() => cambiarImagenOn('platos_fuertes',image_heladochocolate)}  
+                onMouseOut={() => cambiarImagenOff('platos_fuertes',image_principal)}>
+                  Helado Chocolate</a></li>
+
+                <li>
+                <a href="#"
+                onMouseOver={() => cambiarImagenOn('platos_fuertes',image_semifredo)}  
+                onMouseOut={() => cambiarImagenOff('platos_fuertes',image_principal)}>
+                  Semifredo</a></li>
+                
+                <li>
+                <a href="#"
+                onMouseOver={() => cambiarImagenOn('platos_fuertes',image_gelatotartufo)}  
+                onMouseOut={() => cambiarImagenOff('platos_fuertes',image_principal)}>
+                  Gelato al Tartufo</a></li>
+                  
+                <li>
+                <a href="#"
+                onMouseOver={() => cambiarImagenOn('platos_fuertes',image_crepnutella)}  
+                onMouseOut={() => cambiarImagenOff('platos_fuertes',image_principal)}>
+                  Crep de Nutella</a></li>
             </ul>
 
             </td></tr>
             </table>
+            <img id="platos_fuertes" src={image_principal} height="215" width="275" className="imagenes_menu"/>
+            </div>
+            
+          </article>        
           
-          </article>
-
         </main>
+        <br />
         <Footer />
       </>
     );
