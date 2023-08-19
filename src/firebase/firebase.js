021dd8e975eb1c3f02af39fd62ea6a9740f18196
Roberto Lovera <rlovera1cursos@gmail.com>
2023-08-18 20:29:53 -0400

    acceso a bd

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -20,22 +20,46 @@
 
 ////---------------------------------------------------------------------------------------------------------------------
 
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import {getFirestore} from "firebase/firestore"
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBUB1Uvc1TH3jaFdWjxrVXt2CSKe99hm8U",
+//   authDomain: "restaurante-app2.firebaseapp.com",
+//   projectId: "restaurante-app2",
+//   storageBucket: "restaurante-app2.appspot.com",
+//   messagingSenderId: "814297866135",
+//   appId: "1:814297866135:web:d188a04a58a6787201eed2"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app);
+
+
+////--------------------------------------------------------------------------------------------------------------------
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
+//import {getFirestore} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBUB1Uvc1TH3jaFdWjxrVXt2CSKe99hm8U",
-  authDomain: "restaurante-app2.firebaseapp.com",
-  projectId: "restaurante-app2",
-  storageBucket: "restaurante-app2.appspot.com",
-  messagingSenderId: "814297866135",
-  appId: "1:814297866135:web:d188a04a58a6787201eed2"
+  apiKey: "AIzaSyC-rGhpBdPyMK4MX1ef4wuL96ZNlZ6RY8M",
+  authDomain: "restaurante-app3.firebaseapp.com",
+  projectId: "restaurante-app3",
+  storageBucket: "restaurante-app3.appspot.com",
+  messagingSenderId: "902014839792",
+  appId: "1:902014839792:web:87f34fd29962dd35f38b35"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+//export const db = getFirestore(app);
